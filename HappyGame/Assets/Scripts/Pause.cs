ae2ae0d5cdbedd925609c09adeba745b797c3e6f
VictorTirado <arlaquim@gmail.com>
2017-10-06 18:53:46 +0200

    Pause

---
+++ HappyGame/Assets/Scripts/Pause.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+
+            }
+
+            else
+                Time.timeScale = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            if (Time.timeScale == 0)
+            {
+                Time.timeScale = 1;
+            }
+            else
+                Time.timeScale = 1;
+        }
+
+    }
+}
