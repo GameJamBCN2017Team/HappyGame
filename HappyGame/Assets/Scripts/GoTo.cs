131272e28c35b5b196fb01558d0acf39c839303d
Ferran Martin Vilà <ferranmartinvila21@gmail.com>
2017-10-06 15:24:13 +0200

    GoTo & Scape added

---
+++ HappyGame/Assets/Scripts/GoTo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoTo : MonoBehaviour
+{
+
+    public GameObject Player;
+    public float min_distance;
+    public float speed;
+
+    void Start()
+    {
+        Player = GameObject.FindGameObjectsWithTag("Player")[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(Player.transform.position, transform.position);
+        if (dist > min_distance)
+        {
+            Vector3 direction = Player.transform.position - transform.position;
+            direction = Vector3.Normalize(direction) * Time.deltaTime * speed;
+            transform.Translate(direction);
+        }
+    }
+}
---
+++ HappyGame/Assets/Scripts/Scape.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scape : MonoBehaviour
+{
+
+    public GameObject Player;
+    public float min_distance;
+    public float speed;
+
+    void Start()
+    {
+        Player = GameObject.FindGameObjectsWithTag("Player")[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dist = Vector3.Distance(Player.transform.position, transform.position);
+        if (dist < min_distance)
+        {
+            Vector3 direction = Player.transform.position - transform.position;
+            direction = -Vector3.Normalize(direction) * Time.deltaTime * speed;
+            direction.z = 0;
+            transform.Translate(direction);
+        }
+    }
+}
