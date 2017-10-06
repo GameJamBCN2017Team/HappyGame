cd27e7d3ec6258a8d34c4ab99cc4c477fe36ed83
Ferran Martin Vilà <ferranmartinvila21@gmail.com>
2017-10-06 18:44:50 +0200

    Player fire rate implemented

--- HappyGame/Assets/Scripts/PlayerShooting.cs
+++ HappyGame/Assets/Scripts/PlayerShooting.cs
@@ -4,19 +4,26 @@ public class PlayerShooting : MonoBehaviour
 {
     public Rigidbody2D bullet;
     public float power = 1500f;
-
+    public float fire_rate = 0.01f;
+    private float cur_time = 0.0f;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonUp("Fire1"))
+        cur_time += Time.deltaTime;
+
+        if (cur_time > fire_rate)
         {
-            Rigidbody2D instance = Instantiate(bullet, transform.position, transform.rotation) as Rigidbody2D;
 
-            Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector3 directionVector = (target - transform.position).normalized;
-            directionVector.z = 0;
-            instance.AddForce(directionVector * power);
+            if (Input.GetButton("Fire1") || Input.GetButtonDown("Fire1"))
+            {
+                Rigidbody2D instance = Instantiate(bullet, transform.position, transform.rotation) as Rigidbody2D;
+
+                Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Vector3 directionVector = (target - transform.position).normalized;
+                directionVector.z = 0;
+                instance.AddForce(directionVector * power);
+                cur_time = 0.0f;
+            }
         }
-
     }
 }
