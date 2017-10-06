e5b92b000a21c474068064319a278fa62f163251
VictorTirado <arlaquim@gmail.com>
2017-10-06 21:53:52 +0200

    PauseUIFunctionality

--- HappyGame/Assets/Scripts/PauseUI.cs
+++ HappyGame/Assets/Scripts/PauseUI.cs
@@ -1,9 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseUI : MonoBehaviour
 {
+    private AssetBundle MyLoad;
+    private string[] scenePaths;
+    void Start()
+    {
+        //MyLoad = AssetBundle.LoadFromFile("Assets");
+        //scenePaths = MyLoad.GetAllScenePaths();
+
+    }
     void OnGUI()
     {
         GUIStyle name2 = new GUIStyle(GUI.skin.box);
@@ -12,7 +21,13 @@ public class PauseUI : MonoBehaviour
         name3.fontSize = 30;
         GUI.color = Color.green;
         GUI.Box(new Rect(510, 160, 350, 200), "Pause", name2);
-        GUI.Button(new Rect(560, 230, 250, 50), "Back to Menu", name3);
+        if (GUI.Button(new Rect(560, 230, 250, 50), "Back to Menu", name3))
+        {
+            // Application.LoadLevel("Menu");
+            // SceneManager.LoadScene(scenePaths[1], LoadSceneMode.Single);
+            SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+        }
+       
         GUI.Button(new Rect(560, 290, 250, 50), "Exit game", name3);
     }
 
