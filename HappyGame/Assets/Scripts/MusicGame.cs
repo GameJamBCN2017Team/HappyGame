f61b7e96ca31f2149f499dbbcd5c2785e6df80ff
VictorTirado <arlaquim@gmail.com>
2017-10-07 01:23:35 +0200

    Music game

---
+++ HappyGame/Assets/Scripts/MusicGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicGame : MonoBehaviour {
+    public GameObject music1;
+    public GameObject music2;
+	// Use this for initialization
+    void Start()
+    {
+        StartCoroutine("Music");
+    }
+	IEnumerator Music () {
+        print(Time.time);
+        yield return new WaitForSeconds(6f);
+        print(Time.time);
+        music1.SetActive(false);
+        music2.SetActive(true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ HappyGame/Assets/Scripts/SoundMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundMenu : MonoBehaviour {
+    public GameObject projectile;
+    public AudioClip shootSound;
+    private float throwSpeed = 2000f;
+    private AudioSource source;
+    private float volLowRange = .5f;
+    private float volHighRange = 1.0f;
+    // Use this for initialization
+    void Start () {
+        source = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        float vol = 0.5f;
+        source.PlayOneShot(shootSound, vol);
+        GameObject throwThis = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+        throwThis.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 0, throwSpeed));
+    }
+}
