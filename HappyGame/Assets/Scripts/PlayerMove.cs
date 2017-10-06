11cbc70c505b149710cbacc2a84cefb580470a8c
Ferran Martin Vilà <ferranmartinvila21@gmail.com>
2017-10-06 19:46:41 +0200

    Player movement limited with map margins

--- HappyGame/Assets/Scripts/PlayerMove.cs
+++ HappyGame/Assets/Scripts/PlayerMove.cs
@@ -24,8 +24,23 @@ public class PlayerMove : MonoBehaviour
         Vector3 vec = new Vector3(x, y, 0);
         this.GetComponent<Rigidbody2D>().AddForce(vec, ForceMode2D.Force);
         //Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        
+        if (transform.position.x <= -10.0f)
+        {
+            transform.position = new Vector3(-10.0f, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x >= 10.0f)
+        {
+            transform.position = new Vector3(10.0f, transform.position.y, transform.position.z);
+        }
 
-
-        //rb.AddForce(movement);
+        if (transform.position.y <= -4.5f)
+        {
+            transform.position = new Vector3(transform.position.x, -4.5f, transform.position.z);
+        }
+        else if (transform.position.y >= 4.5f)
+        {
+            transform.position = new Vector3(transform.position.x, 4.5f, transform.position.z);
+        }
     }
 }
