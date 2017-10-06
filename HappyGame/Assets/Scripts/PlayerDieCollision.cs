c7b05acc3a31e05d33e8cfded951d9936a59a090
Ferran Martin Vilà <ferranmartinvila21@gmail.com>
2017-10-06 15:52:08 +0200

    Prefabs & Tags & Collision detection update

---
+++ HappyGame/Assets/Scripts/PlayerDieCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDieCollision : MonoBehaviour {
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "Enemy")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
