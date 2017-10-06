d05575800e6cb8eab79a2e608f5cbcc21b8e06f0
manelmourelo <manelmourelo.1@gmail.com>
2017-10-06 17:27:51 +0200

    Score

---
+++ HappyGame/Assets/Scripts/Score.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+    public uint score = 0;
+    void OnGUI()
+    {
+        GUIStyle myScore = new GUIStyle(GUI.skin.box);
+        myScore.fontSize = 50;
+        GUI.color = Color.red;
+        GUI.Box(new Rect(10, 10, 200, 60), "Score: " + score, myScore);
+    }
+}
