4cf7cd5e61f269a56e17a472e18a3d540a5e01e4
Ferran Martin Vilà <ferranmartinvila21@gmail.com>
2017-10-07 15:38:59 +0200

    Timer stops when player dies

--- HappyGame/Assets/Timer.cs
+++ HappyGame/Assets/Timer.cs
@@ -6,18 +6,22 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour {
 
     Text text;
-
+    PlayerDieCollision p_die_script;
+        
     private float timer;
-    //Text text 
+
+    
 	// Use this for initialization
 	void Start () {
         text = GetComponent<Text>();
-        timer = 0.0f;	
+        timer = 0.0f;
+        p_die_script = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerDieCollision>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        timer += Time.deltaTime;
+        if (p_die_script.dead == false) timer += Time.deltaTime;
+
         float val = Mathf.Round(timer * 100.0f) / 100.0f;
         text.text = val.ToString();
     }
